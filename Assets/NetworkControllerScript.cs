b08fb4cccbc363b9198d275218d3f4124ad1b152
Howard Lee <howardchlee@gmail.com>
2013-12-21 16:23:03 -0800

    Initial Commit, object syncs, but fails to create

---
+++ Assets/NetworkControllerScript.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int MaxConnections = 32;
+	public int Port = 5001;
+	public string GameTypeName = "howardchlee_unique_gamestring";
+
+	public GameObject prefab;
+
+	private bool refreshing = false;
+
+	private HostData[] AvailableGames = new HostData[0];
+
+	public void CreateGame()
+	{
+		Network.InitializeServer (this.MaxConnections, this.Port, !Network.HavePublicAddress());
+		Debug.Log("Server Created!");
+		MasterServer.RegisterHost(GameTypeName, "game1", "testing!");
+	}
+
+	public void RefreshHostList()
+	{
+		Debug.Log ("Searching for Games...");
+		MasterServer.RequestHostList(GameTypeName);
+		this.refreshing = true;
+	}
+
+	// message
+	public void OnMasterServerEvent(MasterServerEvent mse)
+	{
+		if(mse == MasterServerEvent.RegistrationSucceeded)
+		{
+			Debug.Log ("Registered Server!");
+		}
+	}
+
+	// message 
+	public void OnServerInitialized()
+	{
+		InstantiatePlayerObject();
+	}
+
+	public void InstantiatePlayerObject()
+	{
+		GameObject newPlayer = new GameObject();
+		newPlayer.AddComponent<PlayerScript>();
+		newPlayer.name = "Player";
+		GameObject newPlayerObject = (GameObject) Network.Instantiate(prefab, prefab.transform.position, Quaternion.identity, 0);
+		newPlayer.GetComponent<PlayerScript>().setPlayerObject(newPlayerObject);
+	}
+
+	public void OnGUI()
+	{
+		if(!Network.isClient && !Network.isServer)
+		{
+			if(GUI.Button(new Rect(10, 10, 120, 40), "New Game"))
+			{
+				CreateGame ();
+			}
+			if(GUI.Button (new Rect(10, 60, 120, 40), "Find Games"))
+			{
+				RefreshHostList ();
+			}
+
+			if(this.refreshing)
+			{
+				if(MasterServer.PollHostList().Length > 0)
+				{
+					refreshing = false;
+					this.AvailableGames = MasterServer.PollHostList();
+					Debug.Log ("Found Available Games.");
+				}
+			}
+
+			for(int i = 0; i < this.AvailableGames.Length; i++)
+			{
+				if(GUI.Button (new Rect(20, 110 + i*50, 150, 40), this.AvailableGames[i].gameName))
+				{
+					Network.Connect (this.AvailableGames[i]);
+					GameObject g = (GameObject)Network.Instantiate(prefab, new Vector3(10, 10, -0.5f), Quaternion.identity, 0);
+					Debug.Log(g.ToString ());
+					//InstantiatePlayerObject();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private GameObject playerObject = null;
+	private bool inPlay = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void setPlayerObject(GameObject playerObject)
+	{
+		this.playerObject = playerObject;
+		this.inPlay = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!this.inPlay)
+			return;
+		if(!playerObject.networkView.isMine)
+			return;
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			playerObject.transform.Translate(-20f*Time.deltaTime, 0, 0);
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			playerObject.transform.Translate (20f*Time.deltaTime, 0,0);
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			playerObject.transform.Translate (0, 20f*Time.deltaTime, 0);
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			playerObject.transform.Translate(0, -20f*Time.deltaTime, 0);
+		}
+
+		if(playerObject.transform.position.x < -11)
+		{
+			playerObject.transform.position = new Vector3(-11, playerObject.transform.position.y, -0.5f);
+		}
+		else if(playerObject.transform.position.x > 11)
+		{
+			playerObject.transform.position = new Vector3(11, playerObject.transform.position.y, -0.5f);
+		}
+
+		if(playerObject.transform.position.y < -11)
+		{
+			playerObject.transform.position = new Vector3(playerObject.transform.position.x, -11, -0.5f);
+		}
+		else if(playerObject.transform.position.y > 11f)
+		{
+			playerObject.transform.position = new Vector3(playerObject.transform.position.x, 11f, -0.5f);;
+		}
+
+
+	}
+}
